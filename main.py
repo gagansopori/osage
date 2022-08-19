6c50f5ed2a05d8b9d1ce9f32c3213c9782b58c20
Gagan Sopori <9592538+gagansopori@users.noreply.github.com>
2022-08-19 00:12:11 -0500

    inital commit for temp

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+
+from src.modules.temp_pressure_humidity.BME280Measurements import TemperaturePressureHumidity
+
+file_name = "temp_readings.txt"
+
+
+def main():
+    tmp = TemperaturePressureHumidity()
+    while True:
+        temp_temp = tmp.measure_temperature()
+        write_to_file(temp_temp)
+        time.sleep(10)
+
+
+def write_to_file(temp_readings):
+    with open(file_name, "a") as temp_data:
+        current_time = time.asctime()
+        write_text = f"{current_time} - Raw Temperature: {temp_readings.raw_temperature} & CPU Temperature: {temp_readings.cpu_temperature}\n "
+        temp_data.write(write_text)
+        temp_data.close()
+
+
+if __name__ == '__main__':
+    main()
