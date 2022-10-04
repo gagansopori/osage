ea5ef7682bc05b4dd3d106d69cdb29f8b6c72d12
Gagan Sopori <9592538+gagansopori@users.noreply.github.com>
2022-10-04 15:22:34 -0500

    Update GasPollutionModel.py

--- src/modules/gas_pollution/GasPollutionModel.py
+++ src/modules/gas_pollution/GasPollutionModel.py
@@ -2,12 +2,15 @@ class GasPollutionModel:
     def __init__(self):
         self._ads_nh3ammonia: float = 0.0
         self._nh3ammonia_ppm: float = 0.0
+        self._nh3ammonia_init: float = 0.0
 
         self._ads_oxidizing: float = 0.0
         self._oxidizing_ppm: float = 0.0
+        self._oxidizing_init: float = 0.0
 
         self._ads_reducing: float = 0.0
         self._reducing_ppm: float = 0.0
+        self._reducing_init: float = 0.0
 
     @property
     def ads_nh3ammonia(self):
@@ -26,6 +29,14 @@ class GasPollutionModel:
         self._nh3ammonia_ppm = nh3_ppm
 
     @property
+    def nh3ammonia_init(self):
+        return self._nh3ammonia_init
+
+    @nh3ammonia_init.setter
+    def nh3ammonia_init(self, nh3_init):
+        self._nh3ammonia_ppm = nh3_init
+
+    @property
     def ads_oxidizing(self):
         return self._ads_oxidizing
 
@@ -42,6 +53,14 @@ class GasPollutionModel:
         self._oxidizing_ppm = oxd_ppm
 
     @property
+    def oxidizing_init(self):
+        return self._oxidizing_init
+
+    @oxidizing_init.setter
+    def oxidizing_init(self, oxd_init):
+        self._oxidizing_init = oxd_init
+
+    @property
     def ads_reducing(self):
         return self._ads_reducing
 
@@ -56,3 +75,11 @@ class GasPollutionModel:
     @reducing_ppm.setter
     def reducing_ppm(self, red_ppm):
         self._reducing_ppm = red_ppm
+
+    @property
+    def reducing_init(self):
+        return self._reducing_init
+
+    @reducing_init.setter
+    def reducing_ppm(self, red_init):
+        self._reducing_init = red_init
