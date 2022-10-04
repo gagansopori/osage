e62e57f73283a3d656a58cd0a734dfeeacf9dec8
Gagan Sopori <9592538+gagansopori@users.noreply.github.com>
2022-10-03 22:05:25 -0500

    Update GasPollutants.py

--- src/modules/gas_pollution/GasPollutants.py
+++ src/modules/gas_pollution/GasPollutants.py
@@ -24,7 +24,7 @@ class GasPollutants:
         self.ads_1015 = ADS1015(i2c_addr=0x49)
         self.ads_1015.set_mode('single')
         self.ads_1015.set_programmable_gain(6.148)
-        self.ads_1015.set_sample_rate(128)
+        self.ads_1015.set_sample_rate(1600)
 
         # init the GPIO for MICS-6814 sensor
         # GPIO.setwarnings(False)
