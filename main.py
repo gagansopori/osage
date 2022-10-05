c19dcc6057ed8097c2a9416e22d9c4efa4c9cb6c
Gagan Sopori <9592538+gagansopori@users.noreply.github.com>
2022-10-04 22:41:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
         headers = [f"TimeStamp", f"BME Temp", f"CPU Temp",
                    f"TMP Temp", f"BME Humidity", f"BME Pressure",
                    f"LTR Lux", f"LTR Prox",
-                   f"Oxidizing", f"Reducing", f"Ammonia",
+                   # f"Oxidizing", f"Reducing", f"Ammonia",
                    f"Oxidizing (ppm)", f"Reducing (ppm)", f"Ammonia (ppm)"]
 
         with open(csv_file, 'w') as init_csv:
@@ -57,7 +57,7 @@ def write_to_csv(temp_readings, lux_prox, gas_poll):
     row_data = [f"{current_time}", f"{temp_readings.bme_temperature:.3f}", f"{temp_readings.cpu_temperature}",
                 f"{temp_readings.tmp_temperature:.3f}", f"{temp_readings.raw_humidity:.3f}",
                 f"{temp_readings.raw_pressure:.3f}", f"{lux_prox.lux:.3f}", f"{lux_prox.proximity:.3f}",
-                f"{gas_poll.ads_oxidizing:.3f}", f"{gas_poll.ads_reducing:.3f}", f"{gas_poll.ads_nh3ammonia:.3f}",
+                # f"{gas_poll.ads_oxidizing:.3f}", f"{gas_poll.ads_reducing:.3f}", f"{gas_poll.ads_nh3ammonia:.3f}",
                 f"{gas_poll.oxidizing_ppm:.3f}", f"{gas_poll.reducing_ppm:.3f}", f"{gas_poll.nh3ammonia_ppm:.3f}"]
     with open(csv_file, 'a') as env_data:
         writer = csv.writer(env_data)
