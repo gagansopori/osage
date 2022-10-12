0f7c0c747f28955ec7f93379393e81aedc025de6
Gagan Sopori <9592538+gagansopori@users.noreply.github.com>
2022-10-12 14:01:27 -0500

    Update TemperaturePressureHumidity.py

--- src/modules/temperature_pressure_humidity/TemperaturePressureHumidity.py
+++ src/modules/temperature_pressure_humidity/TemperaturePressureHumidity.py
@@ -3,8 +3,10 @@
 1. Use the BME-280 sensor to measure indoor pressure & humidity levels at 5 second intervals
 2. Use the BME-280 sensor to measure the ambient temperature at 5 second intervals simultaneously with a TMP-36 sensor
 """
+import time
+
 from . import BME280
-# from ..gas_pollution import ADS1015
+from ..gas_pollution import ADS1015
 
 from src.modules import CPU_TEMPERATURE_FILE, TMP_36
 from src.modules.temperature_pressure_humidity.TemperaturePressureHumidityModel import TemperaturePressureHumidityModel
@@ -34,10 +36,10 @@ class TemperaturePressureHumidity:
         self.bme280.setup('forced')
 
         # init the ads1115 chip to control TMP-36 sensor
-        # self.ads1015 = ADS1015(i2c_addr=0x49)
-        # self.ads1015.set_mode('single')
-        # self.ads1015.set_programmable_gain(4.096)
-        # self.ads1015.set_sample_rate(128)
+        self.ads1015 = ADS1015(i2c_addr=0x49)
+        self.ads1015.set_mode('single')
+        self.ads1015.set_programmable_gain(4.096)
+        self.ads1015.set_sample_rate(128)
 
         # init the model class
         self.environment = TemperaturePressureHumidityModel()
@@ -52,8 +54,8 @@ class TemperaturePressureHumidity:
         self.environment.raw_pressure = p1
         self.environment.raw_humidity = h1
 
-        # tmp1 = self.measure_tmp36_values(TMP_36)
-        # self.environment.tmp_temperature = tmp1
+        tmp1 = self.measure_tmp36_values(TMP_36)
+        self.environment.tmp_temperature = tmp1
 
         self.environment.cpu_temperature = get_cpu_temperature()
 
@@ -62,7 +64,9 @@ class TemperaturePressureHumidity:
     def measure_bme280_values(self):
         return self.bme280.update_sensor()
 
-    # def measure_tmp36_values(self, channel_name) -> float:
-    #     voltage = self.ads1015.get_voltage(channel_name)
-    #     tmp_36 = 100 * (voltage - 0.5)
-    #     return tmp_36
+    def measure_tmp36_values(self, channel_name) -> float:
+        voltage = self.ads1015.get_voltage(channel_name)
+        time.sleep(1)
+        voltage = self.ads1015.get_voltage(channel_name)
+        tmp_36 = 100 * (voltage - 0.5)
+        return tmp_36
